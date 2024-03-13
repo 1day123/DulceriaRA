376baa13b51091894243e1bd08a612e246a88ead
DAYRA SHARAY CORAZA ROLDAN <l20240027@smartin.tecnm.mx>
2024-03-12 21:45:37 -0600

    DoceavoCommit

--- Cliente/inicio.php
+++ Cliente/inicio.php
@@ -10,83 +10,54 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
     <link rel="stylesheet" href="css/misestilos.css">
-    <title>Hello, world!</title>
+    <title>inicio</title>
 </head>
 
 <body>
-    <div class="container-flex" id="fondocinicio">
-        <div class="row" id="barrainicio">
+    <div class="container-fluid">
+        <div class="row" id="barrac1">
             <div class="col">
                 <img src="img/logosfs.png" alt="" id="logo">
             </div>
 
-
             <div class="col">
                 <nav class="nav navbar-expand-lg" id="menuc1">
-                    <a class="nav-link" href="#">Inicio</a>
+                    <a class="nav-link" href="Cliente/inicio.php">Inicio</a>
                     <a class="nav-link" href="#">Categorias</a>
                     <a class="nav-link" href="#">Mis compras</a>
                     <a class="nav-link" href="#">Buscar</a>
                     <a class="nav-link" href="../index.php">Salir</a>
                 </nav>
+
+            </div>
+
+        </div>
+        <div class="row" id="cajacards">
+            <div class="cards">
+                <div class="imagen" id="imagen1"></div>
+                <h4 class="titutocars">Dulces de fruta</h4>
+            </div>
+            <div class="cards">
+                <div class="imagen" id="imagen2"></div>
+                <h4></h4>
+            </div>
+            <div class="cards">
+                <div class="imagen" id="imagen3"></div>
+                <h4></h4>
+            </div>
+            <div class="cards">
+                <div class="imagen" id="imagen4"></div>
+                <h4></h4>
+            </div>
+            <div class="cards">
+                <div class="imagen" id="imagen5"></div>
+                <h4></h4>
             </div>
         </div>
-<div class="row" id="difuminadoinicio">
-
-    <div class="row" id="tituloi">
-        <h4 id="preguntainicio">¿Que deceas comprar?</h4>
-
-    </div>
-    <div class="row">
-        
-
-  
-<section class="menucards">
-
-    <div class="carta1" >
-      <img src="img/dulcedefruta.jpg" alt=""  id="imgcard">
-       <h5 id="tituloscards">Dulces de frutas</h5>
-       </div>
-       <div class="carta1">
-       <img src="img/dulce de leche.jpg" alt="" class="card2" id="imgcard">
-       <h5 id="tituloscards">Dulces de leche</h5>
-       </div>
-       <div class="carta1">
-       <img src="img/nuecesysemillas.jpg" alt="" class="card3" id="imgcard">
-       <h5 id="tituloscards">Dulces de nueces y semillas</h5>
-       </div>
-       <div class="carta1">
-       <img src="img/picosito.jpg" alt="" class="card4" id="imgcard">
-       <h5 id="tituloscards">Picosito</h5>
-       </div>
-       <div class="carta1">
-       <img src="img/Dulcesconvento.jpg" alt=""class="card5" id="imgcard">
-       <h5 id="tituloscards">Dulces de convento</h5>
-       </div>
-   
-
-</section>
-
-
-
-
-
-
-
-
     </div>
 
-</div>
 
 
-
-
-
-
-
-
-
-    </div>
 
     <!-- Optional JavaScript; choose one of the two! -->
 
